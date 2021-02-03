038a5adc29cca230c6074fc0099a32ba16b84efa
Andrew M <34948954+amullinnix@users.noreply.github.com>
2021-02-02 22:03:18 -0600

    Initial Spring Boot Application with Gradle Build System

---
+++ src/main/java/edu/uno/advcomparch/AdvCompArchApplication.java
@@ -0,0 +1,13 @@
+package edu.uno.advcomparch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvCompArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvCompArchApplication.class, args);
+	}
+
+}
